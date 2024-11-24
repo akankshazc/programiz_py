aead080ec2d320feb39e6fbb1d60c7f3704617cc
Akanksha <akankshazc23@icloud.com>
2024-11-24 23:18:04 +0530

    check if key in dict

---
+++ 4_data_types/16_key_dict.py
@@ -0,0 +1,7 @@
+# Progrma to check if a key is present in the dictionary
+
+def check_key(my_dict, key):
+    if key in my_dict:
+        return "Key is present"
+    else:
+        return "Key is not present"
