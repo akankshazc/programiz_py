afefd68bf177575ef011403615387a1053ad8d92
Akanksha <akankshazc23@icloud.com>
2024-11-25 23:03:59 +0530

    split list

---
+++ 4_data_types/17_split_list.py
@@ -0,0 +1,22 @@
+# Program to split a list into two equal halves
+
+def split_list(lst):
+    n = len(lst)
+    return lst[:n//2], lst[n//2:]
+
+
+# Test the function
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+print(split_list(lst))
+
+
+# More than two parts
+def split(list_a, chunk_size):
+
+    for i in range(0, len(list_a), chunk_size):
+        yield list_a[i:i + chunk_size]
+
+
+chunk_size = 2
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(split(my_list, chunk_size)))
