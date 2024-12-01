d984d996fba985e3db51d7877548a3892658bb3f
Akanksha <akankshazc23@icloud.com>
2024-12-01 23:41:05 +0530

    trim whitespace from a string

---
+++ 4_data_types/26_whitespace.py
@@ -0,0 +1,8 @@
+# program to trim the whitespace from the string
+
+def trim_whitespace(string):
+    return string.strip()
+
+
+string = "  Hello, World!  "
+print(trim_whitespace(string))  # Hello, World!
