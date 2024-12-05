cd593d41e812436d36495f8dd1db4032e1d921c1
Akanksha <akankshazc23@icloud.com>
2024-12-05 22:50:56 +0530

    program to capitalize a string

---
+++ 4_data_types/29_capital.py
@@ -0,0 +1,10 @@
+# Program to capitalize the first character of a string
+
+# get the string from the user
+str1 = input("Enter a string: ")
+
+# capitalize the first character
+str1 = str1.capitalize()
+
+# print the modified string
+print("Modified string:", str1)
