d360f415beb71ddcf21124f461e191d73de60882
Akanksha <akankshazc23@icloud.com>
2025-01-08 23:16:04 +0530

    add example program to demonstrate nested functions and closures

---
+++ 8_closures/1_nested.py
@@ -0,0 +1,11 @@
+def greet(name):
+    # inner function
+    def display_name():
+        print("Hi", name)
+
+    # call inner function
+    display_name()
+
+
+# call outer function
+greet("John")
