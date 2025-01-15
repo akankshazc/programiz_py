5eaaa9c852c271cfcd62a003734076a5655a8594
Akanksha <akankshazc23@icloud.com>
2025-01-15 22:13:00 +0530

    add example program to demonstrate a digital clock using time.localtime and time.strftime

---
+++ 10_datetime/21_dig_clock.py
@@ -0,0 +1,12 @@
+import time
+
+# infinite loop (specify iterations before running)
+while True:
+
+    # get current local time as structured data
+    current_time = time.localtime()
+    # format the time in 12-hour clock with AM/PM
+    formatted_time = time.strftime("%I:%M:%S %p", current_time)
+
+    print(formatted_time)
+    time.sleep(1)
